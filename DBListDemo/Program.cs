8a29d92583cac7b9ba8d776440c9109d5f5b6289
Benjamin Davies <benjamindavies@mmc.school.nz>
2020-09-07 15:04:23 +1200

    Add initial framework

---
+++ DBListDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace DBListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            var task = Task.Run(program.Run);
+            task.Wait();
+        }
+
+        private static bool Running = true;
+
+        async Task Run()
+        {
+            while (Running)
+            {
+                Console.Clear();
+                Console.WriteLine("Ben's TODO list app");
+                Console.WriteLine();
+
+                Console.WriteLine("Press 0 to exit");
+                Console.WriteLine("Press 1 to create a new TODO");
+                int result = -1;
+                int.TryParse(Console.ReadKey(true).KeyChar.ToString(), out result);
+
+                switch (result)
+                {
+                    case 0:
+                        Running = false;
+                        break;
+                    case 1:
+                        throw new NotImplementedException();
+                }
+            }
+
+        }
+    }
+}

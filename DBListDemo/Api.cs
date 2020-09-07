85bdc52b584da6db2367777a7dfde5dbe7ce402c
Benjamin-Davies <bentendavies@gmail.com>
2020-09-07 16:17:23 +1200

    Add create method

--- DBListDemo/Api.cs
+++ DBListDemo/Api.cs
@@ -11,6 +11,7 @@ namespace DBListDemo
 
         private HttpClient http;
         private string[] todos;
+        private string todo;
 
         public Api()
         {
@@ -26,8 +27,35 @@ namespace DBListDemo
         private async Task GetTodosAsync()
         {
             var res = await http.GetAsync(TodosUrl);
+            await HandleErrors(res);
             var text = await res.Content.ReadAsStringAsync();
             todos = text.Split('\n', StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public void CreateTodo(string todo)
+        {
+            this.todo = todo;
+            Task.Run(CreateTodoAsync).Wait();
+        }
+
+        private async Task CreateTodoAsync()
+        {
+            var fields = new KeyValuePair<string, string>[]
+            {
+                new KeyValuePair<string, string>("title", todo),
+            };
+            var content = new FormUrlEncodedContent(fields);
+            var res = await http.PostAsync(TodosUrl, content);
+            await HandleErrors(res);
+        }
+
+        private async Task HandleErrors(HttpResponseMessage res)
+        {
+            if (!res.IsSuccessStatusCode)
+            {
+                var text = await res.Content.ReadAsStringAsync();
+                throw new HttpRequestException($"Api returned {res.StatusCode}: {text}");
+            }
         }
     }
 }
--- DBListDemo/Program.cs
+++ DBListDemo/Program.cs
@@ -23,16 +23,22 @@ namespace DBListDemo
                 Console.WriteLine();
                 Console.WriteLine("Press 0 to exit");
                 Console.WriteLine("Press 1 to create a new TODO");
-                int result = -1;
-                int.TryParse(Console.ReadKey(true).KeyChar.ToString(), out result);
+
+                // Accept a keypress from the user
+                int result = int.Parse(Console.ReadKey(true).KeyChar.ToString());
 
                 switch (result)
                 {
-                    case 0:
+                    case 0: // exit
                         Running = false;
                         break;
-                    case 1:
-                        throw new NotImplementedException();
+                    case 1: // create
+                        Console.WriteLine("Please enter a title for your new todo:");
+                        string title = Console.ReadLine();
+
+                        Console.WriteLine("Uploading todo...");
+                        api.CreateTodo(title);
+                        break;
                 }
             }
 

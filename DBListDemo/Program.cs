a2af378fcfd5341ac78a0d1fda9e5f98bd1f25a2
Benjamin Davies <benjamindavies@mmc.school.nz>
2020-09-15 14:42:45 +1200

    Add frontend for delete

--- DBListDemo/Program.cs
+++ DBListDemo/Program.cs
@@ -25,6 +25,7 @@ namespace DBListDemo
                 Console.WriteLine();
                 Console.WriteLine("Press q to quit");
                 Console.WriteLine("Press c to create a new TODO");
+                Console.WriteLine("Press d to delete a TODO");
 
                 // Accept a keypress from the user
                 char key = Console.ReadKey(true).KeyChar;
@@ -35,11 +36,32 @@ namespace DBListDemo
                         break;
                     case 'c': // create
                         Console.WriteLine();
-                        Console.WriteLine("Please enter a title for your new todo:");
+                        Console.WriteLine("Please enter a title for your new todo (empty cancels):");
                         string title = Console.ReadLine();
 
-                        Console.WriteLine("Uploading todo...");
-                        api.CreateTodo(title);
+                        if (title.Length > 0)
+                        {
+                            Console.WriteLine("Uploading todo...");
+                            api.CreateTodo(title);
+                        }
+                        break;
+                    case 'd': // delete
+                        Console.WriteLine();
+                        Console.WriteLine("Please type the number of the todo that you want to delete (empty cancels):");
+                        string number = Console.ReadLine();
+
+                        if (number.Length > 0)
+                        {
+                            if (int.TryParse(number, out int n) && n > 0 && n <= todos.Length)
+                            {
+                                Console.WriteLine($"Deleting todo {number}...");
+                                api.DeleteTodo(n);
+                            }
+                            else
+                            {
+                                Console.WriteLine($"There is no todo matching {number}");
+                            }
+                        }
                         break;
                 }
             }
